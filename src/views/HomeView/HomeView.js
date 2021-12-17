4046562d239f34edd7808cae8ca796d7b3757720
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2021-12-17 15:37:32 +0200

    Переход на фильм с HomeView + картинки одного размера

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,10 +1,9 @@
 import { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
 import { fetchTrending } from '../../services/api-services';
 import PageHeading from '../../components/PageHeading/PageHeadind';
+import MoviesGallery from '../../components/MoviesGallery/MoviesGallery';
 
 export default function HomeView() {
-  const location = useLocation();
   const [movies, setMovies] = useState(null);
 
   useEffect(() => {
@@ -14,20 +13,7 @@ export default function HomeView() {
   return (
     <div>
       <PageHeading text="Trending today" />
-      {movies && (
-        <ul>
-          {movies.map(movie => (
-            <li key={movie.id}>
-              <Link
-                to={`movie/${movie.id}`}
-                state={{ from: location, label: 'Go to Home' }}
-              >
-                {movie.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
+      {movies && <MoviesGallery movies={movies} />}
     </div>
   );
 }
